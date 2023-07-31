b086affd45d70194f9f5cfc6a21357279b046be0
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-07-31 17:01:00 -0400

    finish home page and start mission page

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -1,11 +1,18 @@
+import './mission.css';
+// import coverImg from '../assets/home-cover.png';
+
 const Mission = () => {
     return (
-        <>
-            <h1>This is our mission</h1>
-            <p>have fun!</p>
-        </>
+        <div className="mission">
+            <div className='mission-container-1'>
+                <h1 className="cover-title">Batting Blvd</h1>
+            </div>
+            <div>
+                <h1>This is our mission</h1>
+                <p>Have fun!</p>
+            </div>
+        </div>
     )
 }
 
 export default Mission;
-

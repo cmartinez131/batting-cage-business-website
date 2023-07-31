37ce2ea0671ad4d15df9256b1a5d189f422a7b64
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-07-31 19:37:01 -0400

    take out some repetitive styling

--- src/pages/Features.js
+++ src/pages/Features.js
@@ -1,11 +1,21 @@
+import './features.css';
+
 const Features = () => {
     return (
-        <>
-            <h1>Features</h1>
-        </>
+        <div className="features">
+            <h2>About</h2>
+            <div className="row">
+                <div className="col">1 of 3</div>
+                <div className="col col-wider">2 of 3 (wider)</div>
+                <div className="col">3 of 3</div>
+            </div>
+            <div className="row">
+                <div className="col">1 of 3</div>
+                <div className="col col-even-wider">2 of 3 (even wider)</div>
+                <div className="col">3 of 3</div>
+            </div>
+        </div>
     )
 }
 
 export default Features;
-
-

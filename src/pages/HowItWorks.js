63fc9e11f291471a05f15b4788d45bbee7b13f26
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-09-01 14:50:06 -0400

    add firebase conifiguration for signup and login

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -1,17 +1,17 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import React from 'react';
 import './HowItWorks.css';
 import coverImage from '../assets/home-cover.png';
 
 const HowItWorks = () => {
     return (
-        <Container className="my-container">
-            <Row>
-                <Col>
+        <div className="container">
+            <div className="row">
+                <div className="col">
                     <img src={coverImage} alt="Cover" className="cover-image" />
-                </Col>
-            </Row>
-            <Row className="my-row">
-                <Col>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
                     <h1 className='page-heading'>How it works:</h1>
                     <ol>
                         <li>Visit the Website or Download the App</li>
@@ -27,11 +27,10 @@ const HowItWorks = () => {
                         <li>Confirmation</li>
                         <li>Arrive and Bat</li>
                     </ol>
-                </Col>
-            </Row>
-        </Container>
+                </div>
+            </div>
+        </div>
     );
 }
 
 export default HowItWorks;
-
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,34 @@
+import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
+    const navigate = useNavigate();
+
+    const logIn = async (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        const auth = getAuth(); // consistent with Signup.js
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            alert("Successfully logged in!");
+            navigate('/');
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+
     return (
         <div className="container center-content">
             <h1>Log In</h1>
-            <form className="row row-col">
+            <form className="row row-col" onSubmit={logIn}>
                 <div className="col">
                     <label>
-                        Username:
-                        <input type="text" name="username" required />
+                        Email:
+                        <input type="text" name="email" required />
                     </label>
                 </div>
                 <div className="col">

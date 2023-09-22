4000d16cb6ebf310800bafe067e8284fe671855d
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2023-09-22 15:45:25 -0400

    change navbar dropdown for  small screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,27 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { auth } from '../firebase';
-import '../styles.css'
-import './navbar.css'
-import logo from '../assets/logo.png'
+import '../styles.css';
+import './navbar.css';
+import logo from '../assets/logo.png';
+import { useState } from 'react'; // Import useState
 
 const Navbar = ({ user }) => {
+    const [activeDropdown, setActiveDropdown] = useState(''); // New state variable to manage active dropdown
+
+    const toggleDropdown = (dropdown) => {
+        if (activeDropdown === dropdown) setActiveDropdown(''); // Close if already open
+        else setActiveDropdown(dropdown); // Open the dropdown
+    }
 
     return (
         <nav className="nav">
-
             <Link className='title' to="/">
                 <img src={logo} alt="Batting Blvd Logo" className="logo" />
                 Batting Blvd
             </Link>
-            <div className="dropdown">
-                <button className="dropbtn">The Cage</button>
+
+            <div className={`dropdown ${activeDropdown === 'cage' ? 'active' : ''}`}>
+                <button className="dropbtn" onClick={() => toggleDropdown('cage')}>The Cage</button>
                 <div className="dropdown-content">
                     <Link className='navLink' to="/about">About</Link>
                     <Link className='navLink' to="/features">What We Offer</Link>
@@ -23,36 +30,41 @@ const Navbar = ({ user }) => {
                     <Link className='navLink' to="/blog">Blog</Link>
                 </div>
             </div>
-            <div className="dropdown">
-                <button className="dropbtn">Adults</button>
+
+            {/* Similar modifications for other dropdowns */}
+            <div className={`dropdown ${activeDropdown === 'adults' ? 'active' : ''}`}>
+                <button className="dropbtn" onClick={() => toggleDropdown('adults')}>Adults</button>
                 <div className="dropdown-content">
                     <Link className='navLink' to="/memberships">Memberships</Link>
                 </div>
             </div>
-            <div className="dropdown">
-                <button className="dropbtn">Kids</button>
+
+            <div className={`dropdown ${activeDropdown === 'kids' ? 'active' : ''}`}>
+                <button className="dropbtn" onClick={() => toggleDropdown('kids')}>Kids</button>
                 <div className="dropdown-content">
                     <Link className='navLink' to="/memberships">Memberships</Link>
                 </div>
             </div>
-            <div className="dropdown">
-                <button className="dropbtn">Pro Shop</button>
+
+            <div className={`dropdown ${activeDropdown === 'proShop' ? 'active' : ''}`}>
+                <button className="dropbtn" onClick={() => toggleDropdown('proShop')}>Pro Shop</button>
                 <div className="dropdown-content">
                     <Link className='navLink' to="/pro-shop">Pro Shop</Link>
                 </div>
             </div>
-            <div className="dropdown">
-                <button className="dropbtn">About</button>
+
+            <div className={`dropdown ${activeDropdown === 'about' ? 'active' : ''}`}>
+                <button className="dropbtn" onClick={() => toggleDropdown('about')}>About</button>
                 <div className="dropdown-content">
                     <Link className='navLink' to="/about">About</Link>
                     <Link className='navLink' to="/mission">Our Mission</Link>
                 </div>
             </div>
+
             <div className='loginButtons'>
                 {user ? (
                     <>
                         <span>{user.email}</span>
-                        {/* <Link className="button" to="/profile">Profile</Link> link to profile page */}
                         <button className="button" onClick={() => auth.signOut()}>Logout</button>
                     </>
                 ) : (
@@ -63,7 +75,7 @@ const Navbar = ({ user }) => {
                 )}
             </div>
         </nav>
-    )
+    );
 }
 
 export default Navbar;

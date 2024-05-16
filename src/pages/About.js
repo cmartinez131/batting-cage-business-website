163155f1b150a8c556203018fa40fc208a8279c8
cmartinez131 <75385410+cmartinez131@users.noreply.github.com>
2024-05-15 20:34:59 -0400

    combine about and mission pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,24 +1,19 @@
-import './pageStyles.css'
 import './About.css'
 
 const About = () => {
-
-    const coverImage = 'https://pso.cfaes.ohio-state.edu/sites/ctpp/files/imce/baseball_grass.png';
-
     return (
-        <div className="container">
-            <div className='row center-content cover-container'>
-                <img src={coverImage} alt="Cover" className='img-small' />
-                <div className="cover-text">About</div>
-            </div>
-            <div className="about-content">
-                <p>Founded in 2023, Batting Blvd has grown from a humble start to become a beloved landmark in the community for baseball enthusiasts. Our journey began with a simple idea: to create a space where the love for baseball could be nurtured and shared by people of all ages and abilities.</p>
-                
-                <p>Over the years, we have expanded our offerings and facilities, constantly innovating to provide an unmatched experience for our patrons. From our state-of-the-art batting cages to our comprehensive training programs, every aspect of Batting Blvd is designed with our customers in mind.</p>
+        <div className="about-mission-container">
+            <div className="about-mission-content">
+                <h2 className="about-heading">About Batting Blvd</h2>
+                <p>Founded in 2023, Batting Blvd has quickly become a beloved landmark for baseball enthusiasts. Our journey began with the simple idea of creating a space where the love for baseball could be nurtured and shared by people of all ages and abilities.</p>
+                <p>We have expanded our offerings and facilities, constantly innovating to provide an unmatched experience. From our state-of-the-art batting cages to our comprehensive training programs, every aspect of Batting Blvd is designed with our customers in mind.</p>
+                <p>Our founder, a lifelong baseball fan, envisioned more than just a sports facility. At Batting Blvd, we celebrate the spirit of the game, fostering an environment that encourages learning, improvement, and fun.</p>
 
-                <p>Our founder, a lifelong baseball fan, envisioned a place that went beyond just a sports facility. At Batting Blvd, we celebrate the spirit of the game, fostering an environment that encourages learning, improvement, and most importantly, fun.</p>
-
-                <p>As we continue to grow, our commitment remains the same: to provide a welcoming, high-quality, and accessible baseball experience. We invite you to visit Batting Blvd, where every swing brings a story and every visit is a step closer to hitting your personal home run.</p>
+                <h2 className="about-heading">Our Mission</h2>
+                <p>Our mission is to make baseball accessible, diverse, and enjoyable for everyone. We understand that finding opportunities to play can become more difficult as we grow older, and we're here to change that.</p>
+                <p>Batting Blvd is a welcoming space for all—where enthusiasts can reconnect with the sport they love and beginners can discover the thrill of their first swing. With top-quality facilities and advanced, self-serve batting cages, we provide an environment for everyone to enjoy baseball.</p>
+                <p>We strive to cultivate a vibrant community of baseball lovers through engaging experiences and events. Whether it's a casual session, a family outing, or a special event, Batting Blvd promises memorable experiences beyond the batting cage.</p>
+                <p>Our vision extends beyond promoting the sport. It's about fostering an active lifestyle, strengthening community bonds, and embracing diversity within the game. At Batting Blvd, we believe baseball is more than a game—it's a celebration of resilience, camaraderie, and shared joy.</p>
             </div>
         </div>
     )
